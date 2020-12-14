82024f44f50aa60959437b83d4c68fd6925321e9
paulSugarr <paul.sugar.a@gmail.com>
2020-12-14 16:18:42 +0300

    chg: refactoring.

--- ElectronicVotingServer/Program.cs
+++ ElectronicVotingServer/Program.cs
@@ -1,31 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Net;
 using System.Threading;
-using ElectronicVotingServer;
 using ElectronicVotingServer.Server;
 
 namespace TCPServer
 {
     class Program
     {
-        static ServerModel server;
-        static Thread listenThread;
+        static ServerModel _server;
+        static Thread _listenThread;
         static void Main(string[] args)
         {
             {
                 try
                 {
-                    server = new ServerModel();
-                    listenThread = new Thread(new ThreadStart(server.Listen));
-                    listenThread.Start();
+                    _server = new ServerModel();
+                    _listenThread = new Thread(_server.Listen);
+                    _listenThread.Start();
                 }
                 catch (Exception ex)
                 {
-                    server.Disconnect();
+                    _server.Disconnect();
                     Console.WriteLine(ex.Message);
                 }
             }
